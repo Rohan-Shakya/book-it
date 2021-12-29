0cd2bbb83ea5f368c468d140ef16de4ea56914a5
Rohan-Shakya <rohanshakya254@gmail.com>
2021-12-29 10:24:11 +0545

    created room model and save to database

--- controllers/roomControllers.ts
+++ controllers/roomControllers.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import Room from "../models/room";
 
 const allRooms = (req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({
@@ -7,4 +8,21 @@ const allRooms = (req: NextApiRequest, res: NextApiResponse) => {
   });
 };
 
-export { allRooms };
+// Create new room => /api/rooms
+const newRoom = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const room = await Room.create(req.body);
+
+    res.status(200).json({
+      success: true,
+      room,
+    });
+  } catch (error: any) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+export { allRooms, newRoom };
