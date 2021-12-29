99af6da17f98f9b54d6a256eeccbf33887e0f069
Rohan-Shakya <rohanshakya254@gmail.com>
2021-12-29 10:30:14 +0545

    fetched all rooms

--- controllers/roomControllers.ts
+++ controllers/roomControllers.ts
@@ -1,11 +1,20 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import Room from "../models/room";
 
-const allRooms = (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({
-    success: true,
-    message: "All Rooms",
-  });
+const allRooms = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const rooms = await Room.find();
+    res.status(200).json({
+      success: true,
+      count: rooms.length,
+      rooms,
+    });
+  } catch (error: any) {
+    res.status(400).json({
+      success: true,
+      message: error.message,
+    });
+  }
 };
 
 // Create new room => /api/rooms
--- models/room.ts
+++ models/room.ts
@@ -109,4 +109,4 @@ const roomSchema = new mongoose.Schema({
   },
 });
 
-export default mongoose.model("Room", roomSchema);
+export default mongoose.models?.Room || mongoose.model("Room", roomSchema);
