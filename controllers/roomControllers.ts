0eb496b8657e76761a96dabd9d547f0a5be021ff
Rohan-Shakya <rohanshakya254@gmail.com>
2022-01-08 14:48:21 +0545

    added route for getting single room

--- controllers/roomControllers.ts
+++ controllers/roomControllers.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import Room from "../models/room";
 
+// Get all rooms => /api/rooms
 const allRooms = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const rooms = await Room.find();
@@ -34,4 +35,27 @@ const newRoom = async (req: NextApiRequest, res: NextApiResponse) => {
   }
 };
 
-export { allRooms, newRoom };
+// Get a single room => /api/rooms/:id
+const getSingleRoom = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const room = await Room.findById(req.query.id);
+
+    if (!room) {
+      res.status(404).json({
+        success: true,
+        message: "Room not found with this ID",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      room,
+    });
+  } catch (error: any) {
+    res.status(400).json({
+      success: true,
+      message: error.message,
+    });
+  }
+};
+
+export { allRooms, newRoom, getSingleRoom };
---
+++ pages/api/rooms/[id].ts
@@ -0,0 +1,11 @@
+import nc from "next-connect";
+import dbConnect from "../../../config/dbConnect";
+import { getSingleRoom } from "../../../controllers/roomControllers";
+
+const handler = nc();
+
+dbConnect();
+
+handler.get(getSingleRoom);
+
+export default handler;
